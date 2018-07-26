ca547f3db8e251c03ee063ee4aa13d9ff1b8b228
Natalia Braz <naabraz@gmail.com>
2018-07-25 21:23:54 -0300

    inserting dotenv

--- app/config/config.js
+++ app/config/config.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 module.exports = {
   app: {
     port: 3000,
   },
   database: {
-    url: 'mongodb://ec2-52-67-175-205.sa-east-1.compute.amazonaws.com:17017/metal',
-    name: 'metal',
+    url: process.env.DB_URL,
+    name: process.env.DB_NAME,
   },
 };
