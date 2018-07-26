773b09f36abac9bd44fcbef3964b4a6714f0e74e
Natalia Braz <naabraz@gmail.com>
2018-07-25 23:37:34 -0300

    Cross Env and Development Environment
    Adding Cross Env to set environment info

--- app/config/config.js
+++ app/config/config.js
@@ -5,7 +5,7 @@ module.exports = {
     port: 3000,
   },
   database: {
-    url: process.env.DB_URL,
+    url: process.env.NODE_ENV === 'development' ? process.env.DB_DEV_URL : process.env.DB_URL,
     name: process.env.DB_NAME,
   },
 };
